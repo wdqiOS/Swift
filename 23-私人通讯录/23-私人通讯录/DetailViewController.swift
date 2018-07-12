791db1a2afae1d2d033c1ce29ce18b3cc691b348
3232813737 <3232813737@qq.com>
2018-07-12 23:21:32 +0800

    明细控制器设置数据及返回

--- 23-私人通讯录/23-私人通讯录/DetailViewController.swift
+++ 23-私人通讯录/23-私人通讯录/DetailViewController.swift
@@ -30,7 +30,16 @@ class DetailViewController: UITableViewController {
 
 
     @IBAction func savePerson(_ sender: Any) {
+        // 2、用 UI 更新 person 的内容
+        person?.name = nameText.text
+        person?.phone = phoneText.text
+        person?.title = titleText.text
         
+        // 4、返回上一级内容
+        // 方法的返回值没有使用
+        // _ 可以忽略一切不关心的内容
+       //_ =  navigationController?.popViewController(animated: true)
+        navigationController?.popViewController(animated: true)
     }
     
 }
