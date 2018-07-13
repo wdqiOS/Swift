b16475e708f662cc189e9febaece86cbd93f97a4
3232813737 <3232813737@qq.com>
2018-07-14 00:13:06 +0800

    计算型属性和懒加载的区别，计算型属性类似函数

--- 26-计算型属性/26-计算型属性/Person.swift
+++ 26-计算型属性/26-计算型属性/Person.swift
@@ -34,8 +34,21 @@ class Person: NSObject {
     }
     
     // 只读属性的简写
+    // 又称为‘计算型’属性 -> 本身不保存内容，都是通过计算获得结果
+    // 类似于一个函数
+    // - 没有参数
+    // - 一定有返回值
     var title2:String{
+        print("计算 name \(String(describing: name))")
         return "Mr." + (name ?? "")
     }
+    
+    // 懒加载 title 本质上是一个闭包
+    // 懒加载会在第一次访问的时候，执行，闭包执行结束后，会把结果保存在 title3 中
+    // 后续调用，直接返回title3的内容
+    // 懒加载的属性会分配空间存储值
+    lazy var title3: String? = {
+        return "lazy" + (self.name ?? "")
+    }()
     
 }
--- 26-计算型属性/26-计算型属性/ViewController.swift
+++ 26-计算型属性/26-计算型属性/ViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private lazy var p = Person()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let p = Person()
+//        let p = Person()
+        
+        // 只要调用过一次，懒加载后面的闭包就再也不会执行了！
+        p.title3 = nil
         
         // setter
         p.name = "李哲"
@@ -30,8 +33,15 @@ class ViewController: UIViewController {
         // 不允许修改只读属性
 //        p.title2 = "汪大强"
         
-        
+        print(p.title3)
        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        p.name = "汪大强"
+        print(p.title2)
+        print(p.title3)
+        
     }
 
     override func didReceiveMemoryWarning() {
