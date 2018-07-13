a57cb2bfc1c189ddcc6c32627e5f7c54b8da865e
3232813737 <3232813737@qq.com>
2018-07-14 00:35:41 +0800

    用纯代码加载控制器

---
+++ 28-反射机制/28-反射机制/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  28-反射机制
+//
+//  Created by 汪大强 on 2018/7/14.
+//  Copyright © 2018年 汪大强. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
